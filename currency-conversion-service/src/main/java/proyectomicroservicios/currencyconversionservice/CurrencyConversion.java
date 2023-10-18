72c6e271222fd4fc0aba2ecc3c5f19c78391149d
tortu22993 <enrique.escalante@hotmail.es>
2023-10-18 11:38:48 +0200

    Initial commit

---
+++ currency-conversion-service/src/main/java/proyectomicroservicios/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,84 @@
+package proyectomicroservicios.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculated;
+    private String enviroment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getTotalCalculated() {
+        return totalCalculated;
+    }
+
+    public void setTotalCalculated(BigDecimal totalCalculated) {
+        this.totalCalculated = totalCalculated;
+    }
+
+    public String getEnviroment() {
+        return enviroment;
+    }
+
+    public void setEnviroment(String enviroment) {
+        this.enviroment = enviroment;
+    }
+
+    public CurrencyConversion(){
+
+    }
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculated, String enviroment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
+        this.totalCalculated = totalCalculated;
+        this.enviroment = enviroment;
+    }
+}
---
+++ currency-exchange-service/src/test/java/microservicios/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicios.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
